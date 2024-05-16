2e7535a6028f378a4e5e3ac0272ac4afd1887acf
Saeful25 <msaeful858@gmail.com>
2024-05-16 10:09:38 +0700

    rendering list

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,15 +1,37 @@
 import React from 'react'
 import CardProducts from '../Components/Fragments/CardProducts'
+
+const products = [
+  {
+    id: 1,
+    name: 'Product 1',
+    price: "Rp 100.000",
+    image: '/images/shoes-1.jpg',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus est commodi, ipsa fugiat mollitia voluptates quidem suscipit minus? Aspernatur, dolore.',
+  },
+  {
+    id: 2,
+    name: 'Sepatu snackers',
+    price: "Rp 1.000.000",
+    image: '/images/shoes-1.jpg',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus est commodi',
+  },
+]
+
+
 const Products = () => {
   return (
     <div className='flex justify-center py-5'>
-      <CardProducts>
-        <CardProducts.Header image="/images/shoes-1.jpg"/>
-        <CardProducts.Body title="Sepatu Snackers">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus est commodi, ipsa fugiat mollitia voluptates quidem suscipit minus? Aspernatur, dolore.
+      
+      {products.map ((product) =>
+        <CardProducts>
+        <CardProducts.Header image={product.image}/>
+        <CardProducts.Body name={product.name}>
+          {product.description}
         </CardProducts.Body>
-        <CardProducts.Footer price=" Rp 1.000.000"/>
+        <CardProducts.Footer price={product.price}/>
       </CardProducts>
+      )}  
     </div>
   )
 }
